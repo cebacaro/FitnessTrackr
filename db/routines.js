4031d929910eca0ffd2dd0e9aedd11dd63ee1c65
andre <andre.jacques.pujol@gmail.com>
2023-03-13 14:24:26 -0500

    Working on routine methods, may need to pivot to the routine_activities methods

--- db/routines.js
+++ db/routines.js
@@ -1,12 +1,71 @@
+/* eslint-disable no-useless-catch */
+const { query } = require("express");
 const client = require("./client");
 
-async function createRoutine({ creatorId, isPublic, name, goal }) {}
+async function createRoutine({ creatorId, isPublic, name, goal }) {
+  try {
+    const {
+      rows: [routine],
+    } = await client.query(
+      `
+      INSERT INTO routines("creatorId", "isPublic", name, goal)
+      VALUES($1, $2, $3, $4)
+      RETURNING *;
+      `,
+      [creatorId, isPublic, name, goal]
+    );
+    console.log(routine);
+    return routine;
+  } catch (error) {
+    throw error;
+  }
+}
 
-async function getRoutineById(id) {}
+async function getRoutineById(id) {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const {
+      rows: [routine],
+    } = await client.query(
+      `
+      SELECT * 
+      FROM routines
+      WHERE id=${id}
+      `
+    );
+    return routine;
+  } catch (error) {
+    throw error;
+  }
+}
 
-async function getRoutinesWithoutActivities() {}
+async function getRoutinesWithoutActivities() {
+  try {
+    const { rows } = await client.query(
+      `
+      SELECT *
+      FROM routines
+      `
+    );
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
 
-async function getAllRoutines() {}
+async function getAllRoutines() {
+  try {
+    const { rows: routines } = await client.query(
+      `
+      SELECT *
+      FROM routines
+      JOIN routineactivities ON id=activities."routineId" 
+      `
+    );
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function getAllPublicRoutines() {}
 
@@ -16,7 +75,31 @@ async function getPublicRoutinesByUser({ username }) {}
 
 async function getPublicRoutinesByActivity({ id }) {}
 
-async function updateRoutine({ id, ...fields }) {}
+async function updateRoutine({ id, ...fields }) {
+  const setString = Object.keys(fields)
+    .map((key, idx) => `"${key}"= $${idx + 1}`)
+    .join(", ");
+
+  try {
+    if (setString.length > 0) {
+      const {
+        rows: [routine],
+      } = await client.query(
+        `
+    UPDATE routines
+    SET ${setString}
+    WHERE id=${id}
+    RETURNING *;
+    `,
+        Object.values(fields)
+      );
+      console.log(routine, setString);
+      return routine;
+    }
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function destroyRoutine(id) {}
 

20f3d6b879f1e23d24ea36c5c898c38de64b43c9
andre <andre.jacques.pujol@gmail.com>
2023-03-13 10:07:08 -0500

    Functions for building and dropping tables finished. Func for createing user finished. Working on other user methods

--- db/users.js
+++ db/users.js
@@ -1,27 +1,41 @@
+/* eslint-disable no-useless-catch */
 const client = require("./client");
+const bcrypt = require("bcrypt");
 
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
-  
+  const SALT_COUNT = 10;
+  const hashpassword = await bcrypt.hash(password, SALT_COUNT);
+
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      INSERT INTO users(username, password)
+      VALUES($1, $2)
+      ON CONFLICT (username) DO NOTHING
+      RETURNING *;
+      `,
+      [username, hashpassword]
+    );
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function getUser({ username, password }) {
+async function getUser({ username, password }) {}
 
-}
+async function getUserById(userId) {}
 
-async function getUserById(userId) {
-
-}
-
-async function getUserByUsername(userName) {
-
-}
+async function getUserByUsername(userName) {}
 
 module.exports = {
   createUser,
   getUser,
   getUserById,
   getUserByUsername,
-}
+};

6e77a40b81bf22920a967c69107d60396050b3ca
andre <andre.jacques.pujol@gmail.com>
2023-03-16 10:46:14 -0500

    Working on routines, delete is next

--- api/routines.js
+++ api/routines.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const { tr } = require("faker/lib/locales");
 const router = express.Router();
-const { getAllPublicRoutines } = require("../db");
+const {
+  getAllPublicRoutines,
+  createRoutine,
+  getRoutineById,
+  updateRoutine,
+} = require("../db");
+const { requireUser } = require("./utils");
 
 // GET /api/routines
 router.get("/", async (req, res, next) => {
@@ -16,8 +23,74 @@ router.get("/", async (req, res, next) => {
 });
 
 // POST /api/routines
+router.post("/", requireUser, async (req, res, next) => {
+  // createRoutine({ creatorId, isPublic, name, goal }
+
+  const routineData = {
+    creatorId: req.user.id,
+    isPublic: req.body.isPublic,
+    name: req.body.name,
+    goal: req.body.goal,
+  };
+
+  try {
+    const createdRoutine = await createRoutine(routineData);
+    if (createdRoutine) {
+      res.send(createdRoutine);
+    }
+  } catch (error) {
+    next({
+      name: "PostRoutinesError",
+      message: "Could not create routine",
+    });
+  }
+});
 
 // PATCH /api/routines/:routineId
+router.patch("/:routineId", requireUser, async (req, res, next) => {
+  const { routineId } = req.params;
+  const { isPublic, name, goal } = req.body;
+
+  const updateFields = {};
+
+  if (isPublic) {
+    updateFields.isPublic = isPublic;
+  }
+  if (name) {
+    updateFields.name = name;
+  }
+  if (goal) {
+    updateFields.goal = goal;
+  }
+
+  try {
+    const _checkId = await getRoutineById(routineId);
+    if (!_checkId) {
+      next({
+        name: "RoutineDoesNotExistsError",
+        message: `Routine ${routineId} does not exist`,
+      });
+    } else {
+      const updatedRoutine = await updateRoutine({
+        id: routineId,
+        ...updateFields,
+      });
+      if (updatedRoutine) {
+        res.send(updatedRoutine);
+      } else {
+        next({
+          name: "PatchingRoutineError",
+          message: "Could not update Routine",
+        });
+      }
+    }
+  } catch (error) {
+    next({
+      name: "PatchingRoutineError",
+      message: "Could not update Routine",
+    });
+  }
+});
 
 // DELETE /api/routines/:routineId
 
